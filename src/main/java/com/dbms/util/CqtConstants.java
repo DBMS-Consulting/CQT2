6a4d6f96c47dee8c6be972d1f06262efc35a2a9d
nazgul <vireshwali@gmail.com>
2017-02-20 23:32:08 +0530

    pdated values

--- src/main/java/com/dbms/util/CqtConstants.java
+++ src/main/java/com/dbms/util/CqtConstants.java
@@ -1,8 +1,8 @@
 package com.dbms.util;
 
 public class CqtConstants {
-	public static final String CODE_LIST_TYPE_EXTENSION = "EXTENSION";
-	public static final String CODE_LIST_TYPE_DRUG_PROGRAM = "DRUG_PROGRAM";
+	public static final String CODE_LIST_TYPE_EXTENSION = "LIST_EXTENSION_TYPES";
+	public static final String CODE_LIST_TYPE_PROGRAM = "PROGRAM";
 	public static final String CODE_LIST_TYPE_MEDDRA_VERSIONS = "MEDDRA_VERSIONS";
 	public static final String CODE_LIST_TYPE_WORKFLOW_STATES = "WORKFLOW_STATES";
 	public static final String CODE_LIST_TYPE_PRODUCT = "PRODUCT";
