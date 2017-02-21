d438bf33189194dd0a321abccfe9bf13997cc487
leyla <leylasalim89@gmail.com>
2017-02-21 13:37:21 +0000

    Corrections

--- src/main/java/com/dbms/controller/CreateController.java
+++ src/main/java/com/dbms/controller/CreateController.java
@@ -8,7 +8,7 @@ import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.event.AjaxBehaviorEvent;
 
@@ -24,7 +24,7 @@ import com.dbms.util.exceptions.CqtServiceException;
  * @date Feb 12, 2017 7:34:05 AM
  **/
 @ManagedBean
-@RequestScoped
+@SessionScoped
 public class CreateController implements Serializable {
 
 	private static final long serialVersionUID = -443251941538546278L;
@@ -106,7 +106,7 @@ public class CreateController implements Serializable {
 			cmqBaseService.create(selectedData);
 
 			FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO,
-					"Infos saved", "");
+				"Infos saved", "");
 			FacesContext.getCurrentInstance().addMessage(null, msg);
 		} catch (CqtServiceException e) {
 			LOG.error("Exception occured while creating CmqBase190.", e);
