a74f0e51f296f63055ee3df9529ff77cf30d7414
atrokhym <atrokhym@gmail.com>
2017-04-06 00:08:00 -0400

    few fixes

--- src/main/java/com/dbms/util/SWJSFRequest.java
+++ src/main/java/com/dbms/util/SWJSFRequest.java
@@ -51,26 +51,37 @@ public class SWJSFRequest
 	{
 		HttpServletRequest request = getRequest();
 		String value = (String) request.getAttribute(name);
+		return getGroupListValue(key, value);
+	}
 
-		System.out.println("name: " + name);
+	public static List<String> getGroupListValue(String key, String value)
+	{
+		//value = "CN=OPENCQT_ADMIN,OU=CQT_OU,OU=Applications,OU=Delegated,OU=Groups,DC=pxed,DC=pfizer,DC=com:CN=CQT_Users,OU=CQT_OU,OU=Applications,OU=Delegated,OU=Groups,DC=pxed,DC=pfizer,DC=com:CN=GBL-BTNONColleagues,OU=GBLGroups,OU=Applications,OU=Delegated,OU=Groups,DC=pxed,DC=pfizer,DC=com";
+		//System.out.println("name: " + name);
 		System.out.println("key: " + key);
 		System.out.println("value: " + value);
-		
-		//value = "CN=OPENCQT_ADMIN,OU=CQT_OU,OU=Applications,OU=Delegated,OU=Groups,DC=pxed,DC=pfizer,DC=com:CN=CQT_Users,OU=CQT_OU,OU=Applications,OU=Delegated,OU=Groups,DC=pxed,DC=pfizer,DC=com:CN=GBL-BTNONColleagues,OU=GBLGroups,OU=Applications,OU=Delegated,OU=Groups,DC=pxed,DC=pfizer,DC=com";
 
 		List<String> ret = new ArrayList<String>();
 		
 		if (value != null) {		
 			String[] arr = value.split("[\\s,:]+");
 			for (String s : arr) {
-				if (s.startsWith("CN=")) {
+				if (s.startsWith(key + "=")) {
 					String[] v1 = s.split("=");
 					ret.add(v1[1]);
 				}
 			}
 		}
 		
-		return ret;
+		return ret;		
+	}
+	
+	public static String getGroupListValueAsString(String key, String value) {
+		List<String> groupList = getGroupListValue(key, value);
+		if (groupList.size() > 0)
+			return String.join(", ", groupList);
+		else 
+			return null;
 	}
 	
 	public static String getGroupListAsString(String name, String key) {
@@ -80,6 +91,7 @@ public class SWJSFRequest
 		else 
 			return null;
 	}
+
 	
 	
 //	IAMPFIZERUSERGROUPMEMBERSHIP-----CN=OPENCQT_ADMIN,OU=CQT_OU,OU=Applications,OU=Delegated,OU=Groups,DC=pxed,DC=pfizer,DC=com:CN=CQT_Users,OU=CQT_OU,OU=Applications,OU=Delegated,OU=Groups,DC=pxed,DC=pfizer,DC=com:CN=GBL-BTNONColleagues,OU=GBLGroups,OU=Applications,OU=Delegated,OU=Groups,DC=pxed,DC=pfizer,DC=com 
