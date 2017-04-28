49b137191a36aabb4ff7af25e19b6e001b336b3a
Andrius Mielkus <andrius.mielkus@yandex.com>
2017-04-28 06:09:16 +0200

    IA tree generation performance update

--- src/main/java/com/dbms/util/CmqUtils.java
+++ src/main/java/com/dbms/util/CmqUtils.java
@@ -1,6 +1,13 @@
 package com.dbms.util;
 
+import java.util.LinkedList;
+import java.util.List;
+import org.apache.commons.collections4.ListUtils;
+import org.apache.commons.lang3.StringUtils;
+
 public class CmqUtils {
+    public static int SQL_IN_CLAUSE_SPLIT = 999;
+    
 	public static String getExceptionMessageChain(Throwable throwable) {
 	    StringBuilder sb = new StringBuilder();
 	    boolean firstException = true;
@@ -14,4 +21,16 @@ public class CmqUtils {
 	    }
 	    return sb.toString();
 	}
+    
+    public static String convertArrayToTableWith(List<Long> values, String tableName, String columnName) {
+        // Oracle doc says odciNumberList can accept 32767 arguments but it seems it can only accept 999
+        List<List<Long>> ptvs = ListUtils.partition(values, 999);
+        List<String> subtables = new LinkedList<>();
+        for(List<Long> ptv: ptvs) {
+            subtables.add("select column_value as " + columnName
+                + " from table(sys.odciNumberList(" + StringUtils.join(ptv, ",") + "))");
+        }
+        
+        return "with " + tableName + " as (" + StringUtils.join(subtables, " union all ") + ")";
+    }
 }
