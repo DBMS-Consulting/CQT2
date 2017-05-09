acd85a832b90a9bb86c332615ee583b2f3b436bf
Andrius Mielkus <andrius.mielkus@yandex.com>
2017-05-09 21:01:25 +0200

    dark grey color fix

--- src/main/java/com/dbms/view/IARelationsTreeHelper.java
+++ src/main/java/com/dbms/view/IARelationsTreeHelper.java
@@ -409,7 +409,7 @@ public class IARelationsTreeHelper {
             node.setRowStyleClass("blue-colored");
         else if(CSMQBean.IMPACT_TYPE_ICC.equalsIgnoreCase(cmq.getImpactType())
                 || CSMQBean.IMPACT_TYPE_IPC.equalsIgnoreCase(cmq.getImpactType()))
-            node.setRowStyleClass("grey-colored");
+            node.setRowStyleClass("darkgrey-colored");
     }
     
     public void setTargetCmqRelationNodeStyle(HierarchyNode node, CmqRelationTarget cmqRelationTarget) {
