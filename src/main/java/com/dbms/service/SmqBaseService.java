bce8cb4697bfd3a0eab106c9f4c6a0afa4c33171
Viresh <vwali@sapient.com>
2017-06-22 00:20:34 +0530

    fixed.

--- src/main/java/com/dbms/service/SmqBaseService.java
+++ src/main/java/com/dbms/service/SmqBaseService.java
@@ -152,7 +152,7 @@ public class SmqBaseService extends CqtPersistenceService<SmqBase190> implements
 	public List<SmqRelation190> findSmqRelationsForSmqCode(Long smqCode) {
 		List<SmqRelation190> retVal = null;
 		StringBuilder sb = new StringBuilder();
-		sb.append("from SmqRelation190 c where c.smqCode = :smqCode order by c.smqLevel asc");
+		sb.append("from SmqRelation190 c where c.smqCode = :smqCode order by c.smqLevel asc, c.ptName asc");
 		
 		EntityManager entityManager = this.cqtEntityManagerFactory.getEntityManager();
 		try {
