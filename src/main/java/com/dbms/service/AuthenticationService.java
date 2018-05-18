d59394f00638bdc493053da8ab18179bdce1d0e4
amc <amc0001100@gmail.com>
2018-05-18 10:46:37 -0400

    Revert back from Delete test

--- src/main/java/com/dbms/service/AuthenticationService.java
+++ src/main/java/com/dbms/service/AuthenticationService.java
@@ -156,10 +156,10 @@ public class AuthenticationService {
 	}
 
 	private void setPxedDummyData() {
-		this.userCn = "cougha02";
-		this.userGivenName = "alexander";
-		this.userSurName = "coughlin";
-		this.userEmail = "amc@pfizer.com";
+		this.userCn = "zutshm-dummy";
+		this.userGivenName = "Meenakshi";
+		this.userSurName = "Zutushi";
+		this.userEmail = "mzutushi@pfizer.com";
 		if(this.groupMembershipHeader == null) {
 			//if its not filled from the dummy ldap form
 			this.groupMembershipHeader = PXED_DUMMY_GROUP_MEMBERSHIP;
