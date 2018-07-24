4f2b8c50c1c1738748c43bce8ed2f4d8fb578713
Daniel Camarda <daniel@Daniels-MacBook-Pro.local>
2018-07-24 13:33:45 -0400

    Fixed navigation issue in update module that would not leave details page after unsaved change message

--- src/main/java/com/dbms/controller/CreateController.java
+++ src/main/java/com/dbms/controller/CreateController.java
@@ -687,6 +687,7 @@ public class CreateController implements Serializable {
             if (formToOpen != null && !formToOpen.equals(""))
     				return formToOpen;
             else {
+            		Long count = this.cmqBaseService.findCmqCountByCmqNameAndExtension(detailsFormModel.getExtension(), detailsFormModel.getName());
 				if (formSaved)
 	            		goToWizardNextStep();
             }
@@ -1370,6 +1371,7 @@ public class CreateController implements Serializable {
 				FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO,
 						"List '" + selectedData.getCmqName() + "' is successfully saved.", "");
 				FacesContext.getCurrentInstance().addMessage(null, msg);
+				setFormSaved(true);
 			} else {
 				String errorMsg = "The record already exists for List Name "
 						+ detailsFormModel.getName() + " and Extention "
@@ -1381,6 +1383,7 @@ public class CreateController implements Serializable {
 				FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, errorMsg, "");
 				FacesContext.getCurrentInstance().addMessage(null, msg);
 
+				 
 				return null;
 			}
 		} catch (CqtServiceException e) {
